113d71e0e3014a36855ca7fe5ac70ddf1b256672
ashutosh <ashu95@gmail.com.com>
2025-12-12 20:51:44 +0530

    i have updated main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ resource "aws_vpc" "this" {
 resource "aws_subnet" "this" {
   vpc_id     = aws_vpc.this.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
    map_public_ip_on_launch = true
   tags = {
     Name = "Main"
